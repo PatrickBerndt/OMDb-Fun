72a5d9c4a74b555d118d8efb9d2126610b4dafd2
Patrick Berndt <122051407+PatrickBerndt@users.noreply.github.com>
2023-07-31 21:28:29 +0200

    first listing

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,21 +8,23 @@ import { Component } from '@angular/core';
 export class ContentComponent {
 
   listOfMovies: any;
+  
 
   constructor(){
     this.getInfo();
   }
 
- options = {
+  options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWYwYmYwNTdlZGMzODNkZDRlODIyNTgzMDk4ZDIyOCIsInN1YiI6IjY0YzY2YjY1Y2FkYjZiMDEyNTcwMWZiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-QUnTw5wQ4ZX9rTYlAeVxlAivOPkGsZ7J1cem8wdavQ'
+      Authorization: 'Bearer '+ api_token
     }
   };
+ 
 
   async getInfo(){
-    await fetch('https://api.themoviedb.org/3/movie/upcoming?language=de', this.options)
+    await fetch('https://api.themoviedb.org/3/movie/popular?language=de', this.options)
     .then(response => response.json())
     .then(response => this.listOfMovies = response)
     .catch(err => console.error(err));
